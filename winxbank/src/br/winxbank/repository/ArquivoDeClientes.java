f85728ff4439b9f1b023597ffdd3a91a5e712ae8
Natália Bruno Rabelo <nataliabruno@id.uff.br>
2022-06-20 22:53:47 -0300

    :cat2: o pulo do gato que faltava para leitura de json

--- winxbank/src/br/winxbank/repository/ArquivoDeClientes.java
+++ winxbank/src/br/winxbank/repository/ArquivoDeClientes.java
@@ -19,6 +19,7 @@ import com.google.gson.stream.JsonReader;
 import kong.unirest.JsonNode;
 import kong.unirest.ObjectMapper;
 import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONException;
 import kong.unirest.json.JSONObject;
 import org.json.simple.parser.*;
 
@@ -46,45 +47,48 @@ public class ArquivoDeClientes {
                 JSONArray array2;
                 System.out.println("Array de contas do cliente atual: " + object.getJSONArray("contas")); //print debug
                 array2 = object.getJSONArray("contas");
-                for(int j = i; j < array2.length(); j++){
-                    JSONObject object2 = array2.getJSONObject(i);
+                for(int j = 0; j < array2.length(); j++){
+                    JSONObject object2 = array2.getJSONObject(j);
                     System.out.println(object2); //print debug
                     System.out.println(object2.getString("numeroConta")); //print debug
                     JSONArray array3;
                     System.out.println("Extrato da conta: " + object2.getJSONArray("extrato")); //print debug
                     JSONObject cartaoCreditoJson;
-                    cartaoCreditoJson = object2.getJSONObject("cartaoCredito");
-                    System.out.println(cartaoCreditoJson); //print debug
-                    JSONObject cartaoDebitoJson;
-                    cartaoDebitoJson = object2.getJSONObject("cartao");
-                    System.out.println(cartaoDebitoJson);
-                    array3 = object2.getJSONArray("extrato");
                     Conta contaAtual;
-                    System.out.println(cartaoCreditoJson != null);
-                    if(cartaoCreditoJson != null){
+                    try {
+                        cartaoCreditoJson = object2.getJSONObject("cartaoCredito");
+                        System.out.println(cartaoCreditoJson); //print debug
+                        JSONObject cartaoDebitoJson;
+                        cartaoDebitoJson = object2.getJSONObject("cartao");
+                        System.out.println(cartaoDebitoJson);
+                        array3 = object2.getJSONArray("extrato");
                         contaAtual = new ContaCorrente(object2.getInt("numeroConta"), object2.getDouble("saldo"), new Cartao(cartaoDebitoJson.getInt("numero"), cartaoDebitoJson.getInt("csv")), object2.getDouble("dividaDeEmprestimo"), new CartaoCredito(cartaoCreditoJson.getDouble("fatura"), cartaoCreditoJson.getInt("indexMesDaFatura"), cartaoCreditoJson.getBoolean("faturaPaga"), cartaoCreditoJson.getDouble("limite"), cartaoCreditoJson.getInt("numero"), cartaoCreditoJson.getInt("csv")));
                         System.out.println("Conta corrente foi criada"); //print debug
-                    }else{
+                        
+                    }catch (JSONException e){
+                        JSONObject cartaoDebitoJson;
+                        cartaoDebitoJson = object2.getJSONObject("cartao");
+                        System.out.println(cartaoDebitoJson);
+                        array3 = object2.getJSONArray("extrato");
                         contaAtual = new ContaPoupanca(object2.getInt("numeroConta"), object2.getDouble("saldo"), new Cartao(cartaoDebitoJson.getInt("numero"), cartaoDebitoJson.getInt("csv")), object2.getDouble("dividaDeEmprestimo"));
                         System.out.println("Conta poupanca foi criada"); //print debug
                     }
-                    cliente.setContas(contaAtual);
-                    for(int x = j; x < array3.length(); x++){
-                        JSONObject object3 = array3.getJSONObject(i);
+                    for(int x = 0; x < array3.length(); x++){
+                        JSONObject object3 = array3.getJSONObject(x);
                         System.out.println(object3.getString("mesAtual"));
                         Movimentacao movimentacao = new Movimentacao(object3.getString("mesAtual"), object3.getDouble("dinheiroMovimentado"), object3.getString("tipoDaMovimentacao"));
                         contaAtual.setExtrato(movimentacao);
+                        cliente.setContas(contaAtual);
                     }
 
                 }
                 clientes.add(cliente);
             }
+            RegistroDeClientes.getInstancia().setClientes(clientes);
             //print debug
             for(Cliente cliente : clientes){
                 System.out.println(cliente);
             }
-            RegistroDeClientes.getInstancia().setClientes(clientes);
-
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
